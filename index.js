37df55b916d1a0dac38d4878e108695c7c122c44
babusharif875 <payelbegum96@gmail.com>
2021-03-13 19:14:41 +0600

    Letter-Grade
    1st Project

---
+++ index.js
@@ -0,0 +1,64 @@
+//MY first PROJECT//
+//thanks for visiting//
+//Thanks//
+var input = prompt("Enter your mark of 1 subject")
+
+
+if(input>100){
+
+document.write("Invalid mark")
+
+}
+
+
+else if(input>=80){
+
+document.write("A+")
+
+}
+
+else if(input>=70){
+
+document.write("A")
+
+}
+
+else if(input>=60){
+
+document.write("A-")
+
+}
+
+else if(input>=50){
+
+document.write("B")
+
+}
+
+else if(input>=40){
+
+document.write("C")
+
+}
+
+else if(input>=33){
+
+document.write("D")
+
+}
+else if(input>=0){
+
+document.write("Fail")
+
+}
+
+else{
+    document.write("Invalid input")
+}
+
+
+
+
+
+
+
